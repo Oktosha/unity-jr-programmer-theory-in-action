6382e8bc03a736dc97a40d7338d4145df78edc0c
Daria Kolodzey <kolodaria@gmail.com>
2022-02-28 22:13:11 +0100

    Change build settings

--- Assets/Scripts/InventoryDisplay.cs
+++ Assets/Scripts/InventoryDisplay.cs
@@ -5,11 +5,6 @@ public class InventoryDisplay : MonoBehaviour
 {
     [SerializeField] private Text text;
 
-    private void Start()
-    {
-        text = GetComponent<Text>();
-    }
-
     public void SetCoins(int coins)
     {
         text.text = "Coins: " + coins;
